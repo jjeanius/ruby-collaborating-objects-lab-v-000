43868f347c5120e3cb8c2fe8ade250808270e2b7
Judy McGoldrick <jjeanius@verizon.net>
2018-01-18 20:31:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,33 +30,27 @@ attr_accessor :name, :song
   def self.find_by_name(name)
     @@all.find do |artist|
       artist.name == name
-
     end
   end
 
   def self.create_by_name(name)
-         artist = self.new(name)
-
-         artist.save
-         artist
-
+     artist = self.new(name)
+     artist.save
+     artist
   end
 
-
   def self.find_or_create_by_name(name)
-
     if self.find_by_name(name) == nil
       self.create_by_name(name)
-    else
-      self.find_by_name(name)
+        else
+          self.find_by_name(name)
     end
   end
 
   def print_songs
     songs.each do |song|
-    puts song.name
-   end
+      puts song.name
     end
-
+  end
 
 end
