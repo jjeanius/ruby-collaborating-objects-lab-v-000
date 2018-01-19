fd745c6e2868639b85f8ba7e32303479043e2d2d
Judy McGoldrick <jjeanius@verizon.net>
2018-01-19 02:39:34 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,8 +13,8 @@ class MP3Importer
     Dir.chdir(@path) do | path |
         Dir.glob("*.mp3")
 
+    end
 
-    
     #@files ||= Dir.glob("./spec/fixtures/mp3s").collect{ |f| f.gsub("./spec/fixtures/mp3s") }
   end
 
